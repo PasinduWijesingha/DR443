f1fc99073972b6528f3e705baf5b7a0bc3063ade
Pasindu Wijesinghe <76149619+PasinduWijesingha@users.noreply.github.com>
2023-02-14 05:50:04 +0000

    add ned device to a10.py

--- a10.py
+++ a10.py
@@ -11,41 +11,25 @@
 #     "session_log": "output.txt"
 # }
 
-# Show command that we execute
+# # Show command that we execute
 # command = "show ip"
 # with ConnectHandler(**cisco1) as net_connect:
-    #output = net_connect.send_command(command)
-    # net_connect.enable()
-    # net_connect.send_command_timing("configure terminal")
-    # net_connect.send_command_timing("harmony-controller profile")
-    # net_connect.send_command_timing("deregister")
-    # net_connect.send_command_timing("exit")
-    # net_connect.send_command_timing("no harmony-controller profile")
-    # net_connect.send_command_timing("y")
-    # net_connect.send_command_timing("harmony-controller profile")
-    # net_connect.send_command_timing("host 172.21.10.56 use-mgmt-port port 443")
-    # net_connect.send_command_timing("provider root")
-    # net_connect.send_command_timing("user-name super-admin")
-    # net_connect.send_command_timing("password P@ssw0rd@123")
-    # net_connect.send_command_timing("thunder-mgmt-ip 172.21.10.180")
-    # output = net_connect.send_command_timing("register")
+#     output = net_connect.send_command(command)
 
 
-
-
-
-
-
+# Run this script
 from netmiko import ConnectHandler
 
-a10 = {
-    "device_type": 'a10',
-    "host": "172.21.10.180",
-    "username": "admin",
-    "password": "a10",
+# Device one
+
+a10_device0 = {
+        "device_type": 'a10',
+        "host": "172.21.10.180",
+        "username": "admin",
+        "password": "a10",
 }
 
-net_connect = ConnectHandler(**a10)
+net_connect = ConnectHandler(**a10_device0)
 net_connect.enable()
 net_connect.send_command_timing("configure terminal")
 net_connect.send_command_timing("harmony-controller profile")
@@ -54,11 +38,42 @@ net_connect.send_command_timing("exit")
 net_connect.send_command_timing("no harmony-controller profile")
 net_connect.send_command_timing("y")
 net_connect.send_command_timing("harmony-controller profile")
-net_connect.send_command_timing("host 172.21.10.56 use-mgmt-port port 443")
+net_connect.send_command_timing("host 172.21.10.157 use-mgmt-port port 443")
 net_connect.send_command_timing("provider root")
 net_connect.send_command_timing("user-name super-admin")
 net_connect.send_command_timing("password P@ssw0rd@123")
 net_connect.send_command_timing("thunder-mgmt-ip 172.21.10.180")
 output = net_connect.send_command_timing("register")
 
-print(output)
+print("Output Device1: " + output)
+print("\n")
+
+# Device two
+
+a10_device1 = {
+        "device_type": 'a10',
+        "host": "172.21.10.181",
+        "username": "admin",
+        "password": "a10",
+}
+
+net_connect = ConnectHandler(**a10_device1)
+net_connect.enable()
+net_connect.send_command_timing("configure terminal")
+net_connect.send_command_timing("harmony-controller profile")
+net_connect.send_command_timing("deregister")
+net_connect.send_command_timing("exit")
+net_connect.send_command_timing("no harmony-controller profile")
+net_connect.send_command_timing("y")
+net_connect.send_command_timing("harmony-controller profile")
+net_connect.send_command_timing("host 172.21.10.157 use-mgmt-port port 443")
+net_connect.send_command_timing("provider root")
+net_connect.send_command_timing("user-name super-admin")
+net_connect.send_command_timing("password P@ssw0rd@123")
+net_connect.send_command_timing("thunder-mgmt-ip 172.21.10.181")
+output1 = net_connect.send_command_timing("register")
+
+print("Output Device2: " + output1)
+
+# Device three
+
